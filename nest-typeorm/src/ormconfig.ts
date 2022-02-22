fcf4f66b18b598abee0a70f5c815c71d74f8a3e1
tomoyo519 <tomoyo519@naver.com>
2022-02-23 08:58:16 +0900

    delete cache

--- nest-typeorm/src/ormconfig.ts
+++ nest-typeorm/src/ormconfig.ts
@@ -13,7 +13,7 @@ dotenv.config();
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'localhost',
-  port: 3306,
+  // port: 3307,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
